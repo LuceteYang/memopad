cbe9dd7ef88007d3d0a643a4a2b86e04870495e4
rooney11 <jae6120@naver.com>
2016-08-18 00:39:46 +0900

    서버 구축

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by User on 2016-08-17.
+ */
+import express from 'express';
+import account from './account';
+import memo from './memo';
+
+const router = express.Router();
+router.use('/account', account);
+router.use('/memo', memo);
+
+export default router;
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
     entry: './src/index.js',
@@ -7,29 +8,24 @@ module.exports = {
         path: __dirname + '/public/',
         filename: 'bundle.js'
     },
-
-    devServer: {
-        hot: true,
-        inline: true,
-        host: '0.0.0.0',
-        port: 4000,
-        contentBase: __dirname + '/public/',
+    resolve: {
+        root: path.resolve('./src')
     },
 
     module: {
         loaders: [
             {
                 test: /\.js$/,
-                loaders: ['react-hot', 'babel?' + JSON.stringify({
+                loaders: ['babel?' + JSON.stringify({
                     cacheDirectory: true,
                     presets: ['es2015', 'react']
                 })],
                 exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                loader: 'style!css-loader'
             }
         ]
-    },
-
-    plugins: [
-        new webpack.HotModuleReplacementPlugin()
-    ]
+    }
 };
